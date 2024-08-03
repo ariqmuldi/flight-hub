a47e51e1a9ce4d74f9669494757ce479589f1ac4
Ariq Muldi <ariqmuldi@gmail.com>
2024-08-03 17:32:39 +0300

    Added all main page components

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 cors = CORS(app, origins="*")
@@ -11,6 +12,14 @@ def users():
             "test1", "test2", "test3"
             ] 
         }
+
+# @app.route('/submit-email', methods=['POST'])
+# def submit_email():
+#     data = requests.json()
+#     email = data.get('email')
+#     # Process the email (e.g., save it to a database)
+#     print(f"Received email: {email}")
+#     return jsonify({"message": "Email received!"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
