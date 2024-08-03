6083284924eba2ddf01399c1808dfd39374890a9
Ariq Muldi <ariqmuldi@gmail.com>
2024-08-03 18:05:28 +0300

    Add comments

--- server/main.py
+++ server/main.py
@@ -13,13 +13,17 @@ def users():
             ] 
         }
 
+# Define a route that handles POST requests to the /submit-email endpoint
 @app.route('/submit-email', methods=['POST'])
 def submit_email():
+    # This function is called when a POST request is made to /submit-email
+    # Get the JSON data from the request body
+
     data = request.get_json()  # Get the JSON data from the request
     email = data.get('email')  # Extract the email field
-    # You can now process the email (e.g., save it to a database)
-    print(f"Received email: {email}")
-    return jsonify({"message": "Email received",}), 200
+
+    print(f"Received email: {email}") # Outputs the email to the console 
+    return jsonify({"message": "Email received",}), 200 # Returns JSON response with a message and a status code of 200 (OK) to React
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
