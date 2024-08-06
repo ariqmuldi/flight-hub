63486362d9cae9d323f72ef7dd9df1096ce5c58d
Ariq Muldi <ariqmuldi@gmail.com>
2024-08-06 11:55:02 +0300

    Implement register

--- flight-hub/src/components/UserForm.jsx
+++ flight-hub/src/components/UserForm.jsx
@@ -3,36 +3,109 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/esm/Row';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function UserForm(props) {
+    const [email, setEmail] = useState('');  
+    const [password, setPassword] = useState('');  
+    const [name, setName] = useState(''); 
+    const [message, setMessage] = useState(''); 
+    const [redirectLogin, setRedirectLogin] = useState(null);
+    const saltRounds = 8;
+    const navigate = useNavigate(); // Hook for navigation
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            // Make a POST request to the Flask backend with email and password data
+            const response = await axios.post('http://127.0.0.1:5000/register', 
+                {
+                name : name, 
+                email: email,
+                password : password,
+                }, 
+                {
+                headers: {
+                    'Content-Type': 'application/json', // Set the content type to JSON
+                }
+            });
+
+            console.log(response.data); // Log the response message from the backend. Should be "Email Recieved"
+            setMessage(response.data.message);
+            setRedirectLogin(response.data.redirectLogin);
+
+            console.log(response.data.redirectLogin)
+
+            if (response.data.redirectLogin) {
+                setTimeout(() => {
+                    navigate('/login'); // Redirect to login page
+                    setMessage(''); // Clear message
+                    setRedirectLogin(false); // Reset redirectLogin
+                }, 2500); // Optional delay for user to see the message
+            }
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    function handleEmailChange(e) {
+        setEmail(e.target.value)
+    
+    }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value)
+    }
+
+    function handleNameChange(e) {
+        setName(e.target.value)
+    }
+
     return (
         <Container className="d-flex flex-column align-items-center">
-            <Row className="text-light mt-3 mb-3">
+            <Row className="text-light mt-3">
                 { props.formType === "login" ? <h3> Login To Flight Hub </h3> : <h3> Register To Flight Hub </h3> }
             </Row>
 
+            { redirectLogin === true ? 
+            <Row className="text-danger mt-1">
+            <h6> {message} </h6> 
+            </Row>
+            : 
+            <Row className="text-success mt-1">
+            <h6> {message} </h6> 
+            </Row>
+            }
+            
             <Row>
-            <Form className="w-100">
+            <Form className="w-100" onSubmit={handleSubmit}>
+                { props.formType === "login" ? 
+                null :
+                <Form.Group className="mb-3" controlId="formBasicText" >
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter email" name="name" onChange={handleNameChange}/>
+                </Form.Group>
+                }
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                
+                    <Form.Control type="email" placeholder="Enter name" name="email" onChange={handleEmailChange}/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" name="password" onChange={handlePasswordChange}/>
                 </Form.Group>
-            </Form>
-            </Row>
-
-            <Row>
+            
                 <Button className="buttonEmailForm" variant="primary" type="submit">
                 { props.formType === "login" ? <span> Login </span> : <span> Register </span> }
                 </Button>
+            </Form>
             </Row>
-            
-            
+
         </Container>
     )
 }
--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin, login_user, LoginManager, current_user, logou
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 cors = CORS(app, origins="*")
@@ -13,9 +14,46 @@ login_manager.init_app(app)
 
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = None
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(100))
+    posts = relationship("BlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="posts")
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    
+    #***************Parent Relationship*************#
+    comments = relationship("Comment", back_populates="parent_post")
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    comment_author = relationship("User", back_populates="comments")
+    
+    #***************Child Relationship*************#
+    post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("blog_posts.id"))
+    parent_post = relationship("BlogPost", back_populates="comments")
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/api/users', methods=["GET"])
 def users():
@@ -36,6 +74,30 @@ def submit_email():
 
     print(f"Received email: {email}") # Outputs the email to the console 
     return jsonify({"message": "Email received",}), 200 # Returns JSON response with a message and a status code of 200 (OK) to React
+
+@app.route('/register', methods=["POST"])
+def register():
+    data = request.get_json()
+
+    name = data.get('name')
+    email = data.get('email')
+    password = data.get('password')
+    password_hashed = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+    print(f"Received email: {name}") # Outputs the name to the console    
+    print(f"Received email: {email}") # Outputs the email to the console 
+    print(f"Received email: {password_hashed}") # Outputs the password to the console 
+
+    result = db.session.execute(db.select(User).where(User.email == email))
+    user = result.scalar()
+    if user:
+        return jsonify({"message": "User already exists. Log in instead! Redirecting...", "redirectLogin" : True})
+
+    new_user = User(email=email, name=name, password=password_hashed)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "Succesful!", "redirectLogin" : False}), 200
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
